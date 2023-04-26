a594bfeccf0896aa01a1e249f80e73aaabb76b09
valentin <vbeshlyaga1@gmail.com>
2023-04-26 21:41:04 +0300

    nothing

--- Number_1.cs
+++ Number_1.cs
@@ -1,6 +1,83 @@
-namespace Lab_7;
+/* namespace Lab_7;
 
 public class Number_1
 {
-    
+    public static void Main()
+    {
+        Grant[] students_1 = new Grant[3];
+        students_1[0] = new Grant(1, new int[] { 4, 5, 5, 5, 5 });
+        students_1[1] = new Grant(2, new int[] { 3, 4, 4, 3, 3 });
+        students_1[2] = new Grant(3, new int[] { 3, 4, 4, 3, 5 });
+        Grant[] students_2 = new Grant[3];
+        students_2[0] = new Grant(4, new int[] { 2, 4, 3, 3, 4 });
+        students_2[1] = new Grant(5, new int[] { 4, 4, 4, 4, 5 });
+        students_2[2] = new Grant(6, new int[] { 5, 4, 4, 4, 5 });
+
+        StudSort(students_1);
+        StudSort(students_2);
+
+        Grant[] array;
+        // объединяем два массива
+        array = (Grant[])students_1.Concat(students_2).ToArray();
+        array = (Grant[])StudSort(array);
+
+        Console.WriteLine("Фамилия Сред.балл Стипендия");
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i].ShowGrant();
+        }
+
+        Console.ReadKey();
+    }
+    static Grant[] StudSort(Grant[] students)
+    {
+        for (int i = 0; i < students.Length; i++)
+        {
+            for (int j = i + 1; j < students.Length; j++)
+            {
+                if (students[i].averageMark < students[j].averageMark)
+                {
+                    (students[i], students[j]) = (students[j], students[i]);
+                }
+            }
+        }
+        return students;
+    }
 }
+class Student
+{
+    public int Surname;
+    public double averageMark;
+    public int[] marks;
+    public Student(int Surname, int[] marks)
+    {
+        this.Surname = Surname;
+        this.marks = marks;
+        for (int i = 0; i < marks.Length; i++)
+        {
+            averageMark += marks[i];
+        }
+        averageMark /= marks.Length;
+    }
+}
+
+class Grant : Student
+{
+    public int money;
+    public Grant(int Surname, int[] marks) : base(Surname, marks)
+    {
+        this.Surname = Surname;
+        this.marks = marks;
+        money = 0;
+        for (int i = 0; i < marks.Length; i++)
+        {
+            if (marks[i] > 3) money = 1700;
+            if (marks[i] <= 3) { money = 0; break; }
+        }
+    }
+    public void ShowGrant()
+    {
+        Console.WriteLine($"{Surname}    {averageMark}       {money} руб");
+    }
+}
+*/
--- Number_2.cs
+++ Number_2.cs
@@ -2,5 +2,82 @@ namespace Lab_7;
 
 public class Number_2
 {
-    
+     public static void Main()
+    {
+        Category[] sportsmens = new Category[8];
+        sportsmens[0] = new Category("Гоша", "м", 2.5);
+        sportsmens[1] = new Category("Соня", "ж", 3.1);
+        sportsmens[2] = new Category("Витя", "м", 2.6);
+        sportsmens[3] = new Category("Вова", "м", 4.3);
+        sportsmens[4] = new Category("Маша", "ж", 2.1);
+        sportsmens[5] = new Category("Даня", "м", 5.4);
+        sportsmens[6] = new Category("Саша", "ж", 2.9);
+        sportsmens[7] = new Category("Геля", "ж", 3.2);
+
+        sportsmens = Sort(sportsmens);
+
+        Console.WriteLine("Фамилия Пол Результат Норматив");
+        for (int i = 0; i < sportsmens.Length; i++)
+        {
+            sportsmens[i].Print();
+        }
+
+        Console.ReadKey();
+    }
+
+    static Category[] Sort(Category[] categories)
+    {
+        for (int i = 0; i < categories.Length; i++)
+        {
+            for (int j = i + 1; j < categories.Length; j++)
+            {
+                if (categories[i].Result > categories[j].Result)
+                {
+                    (categories[i], categories[j]) = (categories[j], categories[i]);
+                }
+            }
+        }
+
+        return categories;
+    }
 }
+
+class Sport
+    {
+        public string Surname;
+        public string Gender;
+        public double Result;
+
+        public Sport(string Surname, string Gender, double Result)
+        {
+            this.Surname = Surname;
+            this.Gender = Gender;
+            this.Result = Result;
+        }
+    }
+    class Category : Sport
+    {
+        public string norm;
+        public Category(string Surname, string Gender, double Result) : base(Surname, Gender, Result)
+        {
+            this.Surname = Surname;
+            this.Result = Result;
+            this.Gender = Gender;
+            if (Gender == "ж")
+            {
+                if (Result < 3.2) norm = "Выполнен";
+                else norm = "Не выполнен";
+               
+            }
+            else
+            {
+                if (Result < 2.5) norm = "Выполнен";
+                else norm = "Не выполнен";
+            }
+        }
+        public void Print()
+        {
+            Console.WriteLine($"{Surname}     {Gender}   {Result}       {norm}");
+        }
+    }
+
